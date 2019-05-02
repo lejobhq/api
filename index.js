801b70609fad48f4d9dd58d21cda9add1359b2e7
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2019-05-02 18:01:10 +0800

    Add static assets

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express();
 // Load local environment variables
 require("dotenv").config({ path: ".env" });
 
+// Serve client static files
+const clientPath = "client";
+
+app.use(express.static(clientPath));
+app.get("*", (req, res) => {
+  res.sendFile("/index.html", { root: clientPath });
+});
+
 // Start the server
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
