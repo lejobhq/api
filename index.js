07c8509fd83ea556568a1e965854d19c5980d0f1
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2019-05-04 17:07:09 +0800

    Add Firestore database

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const { OAuth2Client } = require("google-auth-library");
+const Firestore = require("@google-cloud/firestore");
+const FieldValue = require("firebase-admin").firestore.FieldValue;
 
 const app = express();
+
+app.use(express.json());
 
 // Load environment variables
 require("dotenv").config({
   path: process.env.NODE_ENV === "production" ? ".env" : ".env.dev"
 });
 
-app.use(express.json());
+// Initialise Firestore database
+const db = new Firestore({
+  projectId: process.env.GOOGLE_CLOUD_PROJECT,
+  keyFilename: process.env.FIRESTORE_KEY_FILE_PATH
+});
 
 // Auth
 app.post("/auth", async (req, res) => {
@@ -20,18 +28,56 @@ app.post("/auth", async (req, res) => {
       idToken: req.body.token,
       audience: `${CLIENT_ID}.apps.googleusercontent.com`
     });
-    const payload = ticket.getPayload();
-    // TODO
-    console.dir(payload);
+
+    const {
+      email,
+      email_verified,
+      name,
+      picture,
+      given_name,
+      family_name,
+      locale
+    } = ticket.getPayload();
+
+    const usersRef = db.collection("users");
+    usersRef
+      .where("email", "==", email)
+      .get()
+      .then(async snapshot => {
+        let userId;
+        if (snapshot.empty) {
+          // Create a new user
+          const timestamp = FieldValue.serverTimestamp();
+          const newUser = await usersRef.add({
+            email,
+            email_verified,
+            name,
+            picture,
+            given_name,
+            family_name,
+            locale,
+            created_at: timestamp,
+            updated_at: timestamp
+          });
+
+          userId = newUser.id;
+        } else {
+          // Retrieve the user ID from DB
+          userId = snapshot[0].id;
+        }
+
+        // TODO: generate and send JWT
+
+        res.status(200);
+        res.send({ userId });
+      });
   }
 
-  verify()
-    .then(_ => res.send({ ok: true }))
-    .catch(error => {
-      console.error(error);
-      res.status(403);
-      res.send({});
-    });
+  verify().catch(error => {
+    console.error(error);
+    res.status(403);
+    res.send({});
+  });
 });
 
 // Serve client static files
