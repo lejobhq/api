49d20dc4219641ce216932805d0c63faf6666918
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2019-05-19 17:23:38 +0800

    Enable CORS

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const validateSession = require("./middlewares/validate-session");
 
 const app = express();
 
+app.use(helmet());
+app.use(
+  cors({
+    origin: ["https://www.lejobhq.com"],
+    methods: ["GET", "POST", "PUT", "OPTIONS"]
+  })
+);
 app.use(express.json());
 
 // Load environment variables
@@ -24,7 +33,7 @@ app.get("*", (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Listening on port ${port} ...`);
 });
